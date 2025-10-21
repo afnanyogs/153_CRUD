489d30d1ab98e7c7ec266bda4440a2a2e041640f
Afnan Asilah <afnanyogs@gmail.com>
2025-10-21 14:42:28 +0700

    Menghubungkan aplikasi ke database MySQL secara aktif.

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123',
+  database: 'mahasiswa',
+    port: 3309
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:' + err.stack);
+    return;
+  }
+  console.log('Koneksi Berhasil!');
+});
+
