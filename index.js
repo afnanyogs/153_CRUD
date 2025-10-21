38e9c847d1aa60dcf4d57da28fccc4691cf2be93
Afnan Asilah <afnanyogs@gmail.com>
2025-10-21 14:28:38 +0700

    set up express.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = re
+app.getquire("expresss");
+let mysql = require("mysql12");
+const app =express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
